05ff8ac0885493381594d9eba2c593bd6f9fea51
raymeeeee <daniapoluektov@gmail.com>
2024-10-28 17:14:17 +0000

    create Lottery use in main

--- lesson31cw2/lesson31cw2.cpp
+++ lesson31cw2/lesson31cw2.cpp
@@ -1,6 +1,12 @@
-
+#include "Lottery.h"
 
 int main()
 {
-   srand(time(0))
+	srand(time(0));
+	Lottery lottery;
+	lottery.addProduct(new Products::Fun::Toy("Dinosaur", "ToysAB", 359.9, 3, "NN#9438"));
+	lottery.addProduct(new Products::Fun::Toy("Lego Set: Death Star", "Lego", 10000, 12, "NN#1254"));
+	lottery.addProduct(new Products::Sport::Clothing("Backpack", "JackWolfskin", 4220, 16, "Traveling", "M"));
+
+	lottery.generateWinner();
 }
